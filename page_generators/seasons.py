fab88eb7d0dfe23475abaeeb6ce872f38ddb6495
WillNetsky <willnetsky@gmail.com>
2026-01-07 13:12:08 -0800

    started referencing "MFP Stats" instead of "Pinball Stats"

--- page_generators/seasons.py
+++ page_generators/seasons.py
@@ -193,146 +193,8 @@ def generate_season_pages(env, all_series_data):
             player_name_map = {p['playerId']: p['name'] for p in series['players']}
             t_ids = finals_tournament_ids if isinstance(finals_tournament_ids, list) else [finals_tournament_ids]
 
-            for i, tid in enumerate(t_ids):
-                # If there are multiple tournament IDs, we treat them as concurrent rounds (e.g. Round 1 Group A, Round 1 Group B)
-                # UNLESS they are sequential rounds in a bracket.
-                # The user request implies that for 8 players, there are two groups of 4 (Round 1), then a final round (Round 2).
-                # If 'finals_tournament_ids' is a list [id1, id2], it usually means concurrent tournaments or sequential?
-                # In Matchplay, a "finals" might be a single tournament with multiple rounds, OR multiple tournaments.
-                # The current mapping structure suggests a list of IDs for a season.
-                # If it's a list, we need to know if they are parallel or sequential.
-                # Based on the user request: "first two concurrent rounds on the left side, and the finals round on the right side."
-                # This suggests we might have multiple tournaments representing the same "logical" round (e.g. Semis Group A, Semis Group B).
-                
-                # Let's fetch the data first.
-                games_data = fetch_tournament_games(tid, series_status=series['status'])
-
-                if games_data and games_data.get('data'):
-                    groups = defaultdict(lambda: {'arenas': set(), 'players': defaultdict(lambda: {'name': '', 'games': {}, 'total_points': 0})})
-                    tiebreakers = []
-
-                    for game in games_data['data']:
-                        arena_name = game.get('arena', {}).get('name', 'Unknown Arena')
-                        game_set = game.get('set', 0)
-                        
-                        if len(game['playerIds']) == 2:
-                            tiebreakers.append(game)
-                            continue
-
-                        groups[game_set]['arenas'].add(arena_name)
-
-                        for p_idx, player_id in enumerate(game['playerIds']):
-                            groups[game_set]['players'][player_id]['name'] = player_name_map.get(player_id, 'Unknown Player')
-                            points = float(game['resultPoints'][p_idx])
-                            groups[game_set]['players'][player_id]['games'][arena_name] = points
-                            groups[game_set]['players'][player_id]['total_points'] += points
-
-                    round_data = {
-                        'groups': {f'group_{k}': dict(v) for k, v in groups.items()},
-                        'tiebreakers': []
-                    }
-
-                    for game in tiebreakers:
-                        arena_name = game.get('arena', {}).get('name', 'Unknown Arena')
-                        winner_id = game['resultPositions'][0]
-                        loser_id = game['resultPositions'][1]
-                        round_data['tiebreakers'].append({
-                            'winner': player_name_map.get(winner_id, 'Unknown'),
-                            'loser': player_name_map.get(loser_id, 'Unknown'),
-                            'arena': arena_name
-                        })
-                    
-                    # If we have multiple tournament IDs, we treat them as separate "logical" rounds for now,
-                    # but the user wants to group them.
-                    # If the list has 2 IDs, and they are concurrent, they should probably be displayed side-by-side or in the same "column".
-                    # If they are sequential (Semis -> Finals), they should be separate.
-                    # The user says "first two concurrent rounds... and the finals round". This implies 3 entities?
-                    # Or maybe "first two concurrent rounds" means 2 groups in ONE tournament (or 2 tournaments) are the "Semis".
-                    
-                    # Let's assume the list order in finals_mapping reflects the logical flow if they are sequential.
-                    # But if they are concurrent (e.g. 2 separate tournaments for 2 groups), we might need to merge them into "Round 1".
-                    
-                    # Heuristic: If we have multiple IDs, we store them.
-                    # The template currently iterates 'round_1', 'round_2'.
-                    # If we have [id1, id2], are they Round 1 and Round 2? Or Round 1 Group A and Round 1 Group B?
-                    # The user says "if there are 8 players... two groups of 4... top 2 move on".
-                    # This sounds like a standard Matchplay finals format where a single tournament has multiple rounds.
-                    # BUT, if the mapping has a LIST of IDs, it implies multiple tournaments.
-                    
-                    # Let's look at Spring 2024: [148304, 148333].
-                    # If these are "Semis" and "Finals", they are sequential.
-                    # If they are "Group A" and "Group B", they are concurrent.
-                    # Usually, Matchplay finals are one tournament with rounds.
-                    # If the user split them into two tournaments, we need to know.
-                    
-                    # However, the code currently assigns `round_{i+1}`. So id1 -> round_1, id2 -> round_2.
-                    # If the user wants "first two concurrent rounds on the left", maybe they mean
-                    # the first tournament (id1) contains the semi-finals (2 groups), and the second (id2) contains the finals?
-                    # OR, maybe id1 and id2 ARE the concurrent rounds?
-                    
-                    # Let's assume sequential for now (Round 1, Round 2) based on the list index.
-                    # If id1 has 2 groups, they will be in `round_1.groups`.
-                    # If id2 has 1 group (finals), it will be in `round_2.groups`.
-                    
-                    # The user says: "As it is now, the finals table is merged with one of the groups from the first round."
-                    # This implies that maybe we are merging data incorrectly or the template is displaying them confusingly.
-                    # If `finals_data` has `round_1` and `round_2`, the template puts `round_1` in col 1 and `round_2` in col 2.
-                    
-                    # If the "finals table is merged", maybe we are using the same key for different things?
-                    # `groups` dict uses `game_set` (0, 1, 2...) as key.
-                    # If a tournament has multiple groups (sets), they are `group_0`, `group_1`.
-                    
-                    # If the user sees "finals table merged with one of the groups", maybe they mean visually?
-                    # Or maybe the data processing is combining them?
-                    
-                    # Wait, the loop `for i, tid in enumerate(t_ids):` creates `round_{i+1}`.
-                    # So if we have 2 IDs, we get `round_1` and `round_2`.
-                    # They are distinct in `finals_data`.
-                    
-                    # If the user says "finals table is merged with one of the groups from the first round",
-                    # perhaps they mean that within a SINGLE tournament (id1), there are multiple rounds (Semis and Finals)?
-                    # If so, `fetch_tournament_games` returns ALL games.
-                    # We group by `set`. In Matchplay, `set` usually distinguishes groups in a round.
-                    # But how do we distinguish ROUNDS within a tournament?
-                    # Matchplay API `games` usually have a `roundId`.
-                    # If a tournament has Semis AND Finals, they will have different `roundId`s.
-                    # Our current code ignores `roundId` and groups ONLY by `set`.
-                    # THIS IS THE BUG.
-                    # If Semis has Set 0 and Set 1, and Finals has Set 0,
-                    # `groups[0]` will mix Semis Set 0 and Finals Set 0!
-                    
-                    # We need to group by `roundId` (or `round` index) AND `set`.
-                    # But `roundId` is an opaque ID. We need to know the order.
-                    # We can sort games by `startedAt` or `index` to infer rounds, or use `roundId` if we fetch round details.
-                    # `fetch_tournament_games` returns games.
-                    
-                    # Let's refactor to group by `roundId` first.
-                    
-                    # We need to fetch round details to know the order? Or just sort by `startedAt`?
-                    # Games have `startedAt`.
-                    # Let's group by `roundId` and sort rounds by the earliest game start time.
-                    
-                    # New structure:
-                    # finals_data = {
-                    #   'tournament_1': {
-                    #       'rounds': [
-                    #           { 'name': 'Round 1', 'groups': { ... } },
-                    #           { 'name': 'Round 2', 'groups': { ... } }
-                    #       ]
-                    #   }
-                    # }
-                    
-                    # But to keep it simple for the template, let's flatten it to logical rounds.
-                    # If we have multiple tournaments, we can append their rounds.
-                    
-                    # Let's change the loop to process games properly.
-                    
-                    pass # Placeholder to start the refactor block
-
             # Refactored logic for processing finals data
             all_finals_rounds = []
-            
-            t_ids = finals_tournament_ids if isinstance(finals_tournament_ids, list) else [finals_tournament_ids]
             
             for tid in t_ids:
                 games_data = fetch_tournament_games(tid, series_status=series['status'])
@@ -369,16 +231,9 @@ def generate_season_pages(env, all_series_data):
                         arena_name = game.get('arena', {}).get('name', 'Unknown Arena')
                         game_set = game.get('set', 0)
                         
-                        # Tiebreakers usually have 2 players (or fewer than group size), but standard groups can be 3 or 4.
-                        # A better heuristic for tiebreakers might be needed, or just display them as groups.
-                        # For now, let's keep the 2-player check but be careful. 
-                        # Actually, head-to-head finals are 2 players.
-                        # If it's a tiebreaker, it usually has a specific flag or is a separate round?
-                        # Let's assume standard group play for now.
-                        # If it's a tiebreaker round, it often has fewer games/players.
-                        
-                        # Let's treat everything as a group for now to avoid hiding data.
-                        # If we want to separate tiebreakers, we can check if it's a "tiebreaker" round type if API provided it, but we only have games.
+                        if len(game['playerIds']) == 2:
+                            tiebreakers.append(game)
+                            continue
                         
                         groups[game_set]['arenas'].add(arena_name)
                         for p_idx, player_id in enumerate(game['playerIds']):
@@ -388,19 +243,69 @@ def generate_season_pages(env, all_series_data):
                                 groups[game_set]['players'][player_id]['games'][arena_name] = points
                                 groups[game_set]['players'][player_id]['total_points'] += points
                     
+                    for game in tiebreakers:
+                        arena_name = game.get('arena', {}).get('name', 'Unknown Arena')
+                        winner_id = game['resultPositions'][0]
+                        loser_id = game['resultPositions'][1]
+                        # Tiebreakers are not added to groups, but stored separately
+                    
+                    # Determine leaders for each group
+                    for group_id, group_data in groups.items():
+                        players_list = []
+                        for pid, pdata in group_data['players'].items():
+                            players_list.append({'id': pid, 'points': pdata['total_points']})
+                        
+                        # Sort by points descending
+                        players_list.sort(key=lambda x: x['points'], reverse=True)
+                        
+                        # Identify top 2 (and ties)
+                        leaders = set()
+                        if players_list:
+                            # 1st place
+                            first_points = players_list[0]['points']
+                            for p in players_list:
+                                if p['points'] == first_points:
+                                    leaders.add(p['id'])
+                            
+                            # If fewer than 2 leaders (i.e. clear 1st), check 2nd place
+                            if len(leaders) < 2 and len(players_list) > 1:
+                                # Find next highest score
+                                second_points = -1
+                                for p in players_list:
+                                    if p['points'] < first_points:
+                                        second_points = p['points']
+                                        break
+                                
+                                if second_points != -1:
+                                    for p in players_list:
+                                        if p['points'] == second_points:
+                                            leaders.add(p['id'])
+                        
+                        # Mark leaders in the player data
+                        for pid in group_data['players']:
+                            if pid in leaders:
+                                group_data['players'][pid]['is_leader'] = True
+                            else:
+                                group_data['players'][pid]['is_leader'] = False
+
                     all_finals_rounds.append({
                         'groups': {f'group_{k}': dict(v) for k, v in groups.items()},
-                        'tiebreakers': tiebreakers # Empty for now as we treat all as groups
+                        'tiebreakers': [] # Tiebreakers logic needs to be fully implemented if we want to show them
                     })
+                    
+                    # Add tiebreakers back if we processed them
+                    if tiebreakers:
+                         all_finals_rounds[-1]['tiebreakers'] = []
+                         for game in tiebreakers:
+                            arena_name = game.get('arena', {}).get('name', 'Unknown Arena')
+                            winner_id = game['resultPositions'][0]
+                            loser_id = game['resultPositions'][1]
+                            all_finals_rounds[-1]['tiebreakers'].append({
+                                'winner': player_name_map.get(winner_id, 'Unknown'),
+                                'loser': player_name_map.get(loser_id, 'Unknown'),
+                                'arena': arena_name
+                            })
 
-            # Now we have a list of rounds.
-            # We want to display "first two concurrent rounds on the left side, and the finals round on the right side."
-            # This implies we expect 2 logical phases: Semis and Finals.
-            # If `all_finals_rounds` has 2 entries (Round 1, Round 2), we map them.
-            # If it has 1 entry (e.g. just Finals), we map it.
-            
-            # Let's structure `finals_data` for the template.
-            # We'll pass the list of rounds.
             finals_data['rounds'] = all_finals_rounds
 
         tournament_id_to_week_num = {tid: i + 1 for i, tid in enumerate(series['tournamentIds'])}
@@ -410,9 +315,6 @@ def generate_season_pages(env, all_series_data):
         for player_info in series['players']:
             player_id = player_info['playerId']
             
-            # For active seasons, standings might not be fully populated or reliable for 'position'
-            # if no games have been played, but if 1 week is done, they should be there.
-            # However, if a player hasn't played yet, they might not be in standings or have position 0/null.
             player_standing = next((s for s in series['standings'] if s['playerId'] == player_id), None)
             
             qualifying_position = player_standing['position'] if player_standing else 'N/A'
@@ -442,13 +344,6 @@ def generate_season_pages(env, all_series_data):
 
             player_game_stats = game_data['by_player'].get(player_id, defaultdict(int))
 
-            # Only include players who have played at least one week OR if the season hasn't started but they are registered
-            # Actually, for the stats table, we probably want to see everyone registered, 
-            # but if they haven't played, their stats will be 0.
-            # The issue might be that for an active season with 1 week, 
-            # if 'standings' is empty or not updated by Matchplay yet, we might miss data if we rely solely on it.
-            # But we are iterating over series['players'], so we should get everyone.
-            
             season_players_data.append({
                 'playerId': player_id,
                 'name': player_info['name'],
@@ -460,11 +355,9 @@ def generate_season_pages(env, all_series_data):
                 'game_outcomes': player_game_stats
             })
         
-        # Sort by qualifying position. If N/A (not played/ranked), put at end.
-        # For active seasons, position might be dynamic.
         season_players_data.sort(key=lambda x: (
             x['qualifying_position'] if isinstance(x['qualifying_position'], int) else float('inf'),
-            -x['total_raw_points'] # Tie-break with raw points
+            -x['total_raw_points']
         ))
 
         with open(os.path.join(OUTPUT_DIR, f"season_{series_id}.html"), 'w') as f:
